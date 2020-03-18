d58de505699c99849d87660db6476ca421c3d69b
Somesh Kar <somesh.kar@gmail.com>
2020-03-19 03:56:57 +0530

    Change package name

--- pages/api/db.js
+++ pages/api/db.js
@@ -1,6 +1,9 @@
 const { GoogleSpreadsheet } = require('google-spreadsheet')
 
-const creds = require('./client_secret.json')
+const creds = require({
+  client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+  private_key: process.env.GOOGLE_PRIVATE_KEY,
+})
 const sheetId = '1nzXUdaIWC84QipdVGUKTiCSc5xntBbpMpzLm6Si33zk'
 
 const doc = new GoogleSpreadsheet(sheetId)
