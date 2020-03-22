bb33de3167d8ec16f5ec1338169279acf104f341
sibeshkar <sibeshkar@users.noreply.github.com>
2020-03-22 22:05:14 +0530

    foreign worked

--- util/filters/travel.js
+++ util/filters/travel.js
@@ -2,16 +2,16 @@ import hash from 'object-hash'
 import _ from 'lodash'
 import dotProp from 'dot-prop-immutable'
 
-import { plane_node } from '../../images'
+import { plane_abroad_node, plane_local_node } from '../../images'
 
 export const addTravel = (graph, patients) => {
   let locations = {}
 
   for (let patientId in patients) {
-    if (patients[patientId].travel !== null) {
-      patients[patientId].travel.forEach(loc => {
-        if (!locations[hash(loc)]) {
-          locations[hash(loc)] = loc
+    if (patients[patientId].place_attributes !== null) {
+      patients[patientId].place_attributes.forEach(loc => {
+        if (!locations[hash(loc.place)]) {
+          locations[hash(loc.place)] = loc
         }
       })
     }
@@ -22,20 +22,23 @@ export const addTravel = (graph, patients) => {
   for (let loc in locations) {
     let node = {
       id: loc,
-      label: locations[loc],
+      label: locations[loc].place,
       size: 30,
       shape: 'image',
-      image: plane_node,
+      image: locations[loc].is_foreign ? plane_abroad_node : plane_local_node,
     }
     graph = dotProp.set(graph, 'nodes', list => [...list, node])
   }
 
   // Add edges from patient to location
   for (let patientId in patients) {
-    if (patients[patientId].travel !== null && patients[patientId].travel[0]) {
-      patients[patientId].travel.forEach(loc => {
+    if (
+      patients[patientId].place_attributes !== null &&
+      patients[patientId].place_attributes[0]
+    ) {
+      patients[patientId].place_attributes.forEach(loc => {
         let edge = {
-          from: hash(loc),
+          from: hash(loc.place),
           to: patients[patientId].patientId,
           length: 500,
           dashes: true,
